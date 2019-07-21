059bd8af257ce585295e5801e6c6965a007fb378
yangentao <entaoyang@163.com>
2019-07-21 14:24:13 +0800

    OK

--- core/src/main/kotlin/dev/entao/keb/core/HttpContext.kt
+++ core/src/main/kotlin/dev/entao/keb/core/HttpContext.kt
@@ -66,12 +66,7 @@ class HttpContext(val filter: HttpFilter, val request: HttpServletRequest, val r
 		}
 
 	fun fullUrlOf(uri: String): String {
-		val host = request.getHeader("host") ?: ""
-		if (host.isNotEmpty()) {
-			return request.scheme + "://" + request.getHeader("host") + uri
-		} else {
-			return request.scheme + "://" + request.serverName + ":" + request.serverPort + uri
-		}
+		return request.scheme + "://" + request.getHeader("host") + uri
 	}
 
 	fun groupUri(g: KClass<*>): String {
@@ -192,9 +187,11 @@ class HttpContext(val filter: HttpFilter, val request: HttpServletRequest, val r
 	fun fileParts(): List<Part> {
 		return parts().filter { it.submittedFileName != null && it.submittedFileName.isNotEmpty() }
 	}
-	val firstFilePart:Part? get() {
-		return this.fileParts().firstOrNull()
-	}
+
+	val firstFilePart: Part?
+		get() {
+			return this.fileParts().firstOrNull()
+		}
 
 	fun abort(code: Int) {
 		response.sendError(code)
